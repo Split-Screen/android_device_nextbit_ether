8d983e0a16dae858da89567dd7c51b5f89259f62
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2016-05-03 19:01:07 -0400

    ether: PAC Changes
    Change-Id: I095f7ece3737a2504388bb6f30fa28dd077e0996
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- pac.mk
+++ pac.mk
@@ -1,21 +1,17 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1080
-TARGET_SCREEN_WIDTH := 1920
-
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common PAC stuff.
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/nextbit/ether/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_ether
+PRODUCT_NAME := pac_ether
 BOARD_VENDOR := nextbit
 TARGET_VENDOR := nextbit
 PRODUCT_DEVICE := ether
